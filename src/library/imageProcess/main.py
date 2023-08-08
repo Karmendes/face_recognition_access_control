3d75f2c80bbaf03c16910854baffc500c7c3a10c
karmendes <lucaseqt@hotmail.com>
2023-08-08 15:01:27 -0300

    feat: Add functionalities for image processor

--- src/library/imageProcess/main.py
+++ src/library/imageProcess/main.py
@@ -1,8 +1,21 @@
 import cv2
+import numpy as np
 
 class FrameProcessorStrategy:
     def process(self, frame):
         pass
+
+class BufferToArrayStrategy(FrameProcessorStrategy):
+    def process(self, frame):
+        return np.frombuffer(frame, dtype=np.uint8)
+
+class ArrayToFrameStrategy(FrameProcessorStrategy):
+    def process(self, frame):
+        return cv2.imdecode(frame, cv2.IMREAD_COLOR)
+
+class BGRToGray(FrameProcessorStrategy):
+    def process(self, frame):
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 class FrameToBufferStrategy(FrameProcessorStrategy):
     def process(self, frame):
@@ -12,6 +25,13 @@ class FrameToBufferStrategy(FrameProcessorStrategy):
 class BufferToBytesStrategy(FrameProcessorStrategy):
     def process(self, frame):
         return frame.tobytes()
+
+class CropFrame(FrameProcessorStrategy):
+    def __init__(self, coord):
+        self.coord = coord
+    def process(self, frame):
+        X, Y, W, H = self.coord
+        return frame[Y:Y+H, X:X+W]
 
 class FrameProcessor:
     def __init__(self):
@@ -24,10 +44,21 @@ class FrameProcessor:
             processed_frame = strategy.process(processed_frame)
         return processed_frame
 
+################################################################### directors
+
 class FrameProcessorToFilmMaker:
     def __init__(self):
         self.frame_processor = FrameProcessor()
         self.frame_processor.add_strategy(FrameToBufferStrategy())
         self.frame_processor.add_strategy(BufferToBytesStrategy())
+    def run(self,frame):
+        return self.frame_processor.process_frame(frame)
+
+class FrameProcessorToFaceDetector:
+    def __init__(self):
+        self.frame_processor = FrameProcessor()
+        self.frame_processor.add_strategy(BufferToArrayStrategy())
+        self.frame_processor.add_strategy(ArrayToFrameStrategy())
+        self.frame_processor.add_strategy(BGRToGray())
     def run(self,frame):
         return self.frame_processor.process_frame(frame)
